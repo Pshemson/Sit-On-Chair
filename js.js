70b93fb7279392a08456893b8fe8abd0f4c4baa7
Przemek Bednarczyk <pjbednarczyk@gmail.com>
2018-01-05 10:30:52 +0100

    slider in progress

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
     //Rozsuwane menu
     var menu = document.querySelector(".menu:first-of-type");
@@ -26,13 +26,13 @@ document.addEventListener("DOMContentLoaded", function() {
     var title = document.querySelectorAll('.boxes__box');
 
     function hideTitle() {
-        for (var i =0; i < title.length; i++) {
+        for (var i = 0; i < title.length; i++) {
             this.firstElementChild.classList.add('box_hide');
         }
     }
 
     function showTitle() {
-        for (var i=0; i < title.length; i++) {
+        for (var i = 0; i < title.length; i++) {
             this.firstElementChild.classList.remove('box_hide');
         }
     }
@@ -41,6 +41,42 @@ document.addEventListener("DOMContentLoaded", function() {
     box1.addEventListener('mouseout', showTitle);
     box2.addEventListener('mouseover', hideTitle);
     box2.addEventListener('mouseout', showTitle);
+
+//Ruchomy slider
+
+    var sliderContainer = document.querySelector('.slider');
+    var nextSlide = document.querySelector('.nextSlide');
+    var prevSlide = document.querySelector('.prevSlide');
+    var allSlides = [...sliderContainer.querySelectorAll('.slider-content')];
+    var counter = 0;
+
+    prevSlide.style.color = "#c6c6c6";
+
+    nextSlide.addEventListener('click', function(event) {
+        if (counter <= allSlides.length - 2) {
+            allSlides[counter].style.marginLeft = "-100%";
+            counter++;
+            if (counter < allSlides.length - 1) {
+                prevSlide.style.color = "24ba9f";
+            } else {
+                this.style.color = "c6c6c6";
+            }
+        }
+    });
+
+    prevSlide.addEventListener('click', function(event) {
+        if (counter <= (allSlides.length - 1) && counter >= 1) {
+            --counter;
+            allSlides[counter].style.marginLeft = "0";
+            if (counter < allSlides.length && counter >= 1) {
+                nextSlide.style.color = "24ba9f";
+            } else {
+                this.style.color = "c6c6c6";
+            }
+        }
+    });
+
+
 
 
 
